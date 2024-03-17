e6876d328d951e2f5ef9769584f6411b74038433
AminaHaque27 <aminashaque@gmail.com>
2024-03-16 23:15:23 -0500

    Update index.tsx

--- server/client/pages/index.tsx
+++ server/client/pages/index.tsx
@@ -5,11 +5,32 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <Image
-      src="/background.jpg"
-      alt="Green/blue background"
-      layout="fill"
-      objectFit="cover"
-    />
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <Image
+        src="/background.jpg"
+        alt="Green/blue background"
+        layout="fill"
+        objectFit="cover"
+      />
+      <div
+        style={{
+          width: "200px", // Width of the white box
+          height: "200px", // Height of the white box
+          backgroundColor: "white", // Background color of the box
+          position: "absolute", // Positioning the box absolutely inside the relative container
+          top: "50%", // Centering the box vertically
+          left: "50%", // Centering the box horizontally
+          transform: "translate(-50%, -50%)", // Adjusting the exact center position
+          zIndex: 10, // Ensuring the box is above the background image
+        }}
+      ></div>
+    </div>
   );
 }
